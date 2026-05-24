3e2fa6771e71a03ca3f2f4cbd940f2a1a7a5dfa3
AtanasovCode <a_ljupce@yahoo.com>
2026-05-24 22:26:43 +0200

    implement useNavigate and add 'See Details' navigation button

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,22 @@
 import { Button, Card, CardContent, CardMedia, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 //import productImage from '../assets/shop.png';
 
 export default function ProductCard({ product }) {
+
+    const navigate = useNavigate()
+
     return (
         <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
             <CardContent sx={{ flexGrow: 1 }}>
                 <Typography variant="h6">{product.name}</Typography>
                 <Typography variant="body1" sx={{ my: 1 }}>${product.price}</Typography>
+                <Button
+                    variant="contained" fullWidth sx={{ mt: 1 }}
+                    onClick={() => navigate(`/products/${product.id}`)}
+                 >
+                    See Details
+                </Button>
                 <Button variant="contained" fullWidth sx={{ mt: 1 }}>
                     Add to Cart
                 </Button>
