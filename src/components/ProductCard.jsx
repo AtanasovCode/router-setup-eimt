a08f40a8710a0dffde63a86281f0b4266cea4b36
AtanasovCode <a_ljupce@yahoo.com>
2026-05-24 19:39:34 +0200

    install material ui

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,13 @@
+import {Typography} from "@mui/material";
+
+const ProductCard = () => {
+    return (
+        <div>
+            <Typography variant="h4" color="white" sx={{ mb: 4 }}>
+                Product Card
+            </Typography>
+        </div>
+    );
+}
+
+export default ProductCard;
