f28ba1705d30dc5a56daa930c9a8b1271d3aa979
AtanasovCode <a_ljupce@yahoo.com>
2026-05-25 18:11:17 +0200

    create products state & make instance api call

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,21 +1,39 @@
+import { useState, useEffect } from "react";
 import { Container, Grid, Typography } from "@mui/material";
+import instance from "../axios/axios";
 import ProductCard from "../components/ProductCard"
-import products from "../data/products";
 
 
 const ProductList = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        instance.get("/products")
+            .then((response) => setProducts(response.data))
+            .catch((e) => console.error(e))
+    }, [])
+
     return (
         <Container sx={{ py: 5 }}>
             <Typography variant="h4" sx={{ mb: 4 }}>
                 Our Products
             </Typography>
-            <Grid container spacing={3}>
-                {products.map((product) => (
-                    <Grid key={product.id} size={{ xs: 12, sm: 6, md: 3 }}>
-                        <ProductCard product={product} />
+            {
+                products.length > 0 ? (
+                    <Grid container spacing={3}>
+                        {products.map((product) => (
+                            <Grid key={product.id} size={{ xs: 12, sm: 6, md: 3 }}>
+                                <ProductCard product={product} />
+                            </Grid>
+                        ))}
                     </Grid>
-                ))}
-            </Grid>
+                ) : (
+                    <div>
+                        No Products Found :(
+                    </div>
+                )
+           }
         </Container>
     );
 }
