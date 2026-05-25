e8e1c9bb18f8ab3b19af8c97f92e56684bbb914f
AtanasovCode <a_ljupce@yahoo.com>
2026-05-25 23:58:27 +0200

    improve rendering & fetching - convert to using repository layer instead of calling axios instance - add loading state - improve rendering method

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,13 +1,31 @@
+import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Container, Typography, Button, CardMedia, Paper } from "@mui/material";
-import products from "../data/products.js";
+import productsRepository from "../repository/productsRepository";
+
 
 const ProductDetails = () => {
     const { id } = useParams();
     const navigate = useNavigate();
-    const product = products.find((p) => p.id === Number(id));
 
-    if (!product) {
+    const [product, setProduct] = useState()
+    const [notFound, setNotFound] = useState(false)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        productsRepository.findById(id)
+            .then((response) => {
+                setNotFound(false)
+                setProduct(response.data)
+            })
+            .catch((e) => {
+                console.error(e)
+                setNotFound(true)
+            })
+            .finally(() => setLoading(false))
+    }, [id])
+
+    if (notFound) {
         return (
             <Container sx={{ py: 5 }}>
                 <Typography variant="h5">Product not found.</Typography>
@@ -20,13 +38,20 @@ const ProductDetails = () => {
 
     return (
         <Container sx={{ py: 5 }} maxWidth="sm">
-            <Paper sx={{ p: 3 }}>
-                <Typography variant="h4" sx={{ mt: 2 }}>{product.name}</Typography>
-                <Typography variant="h5" sx={{ my: 1 }}>${product.price}</Typography>
-                <Typography variant="body1" sx={{ mb: 3 }}>{product.description}</Typography>
-                <Button variant="contained" sx={{ mr: 1 }}>Add to Cart</Button>
-                <Button variant="outlined" onClick={() => navigate("/")}>Back</Button>
-            </Paper>
+            {
+                loading ?
+                    <div>
+                        Loading
+                    </div>
+                    :
+                    <Paper sx={{ p: 3 }}>
+                        <Typography variant="h4" sx={{ mt: 2 }}>{product.name}</Typography>
+                        <Typography variant="h5" sx={{ my: 1 }}>${product.price}</Typography>
+                        <Typography variant="body1" sx={{ mb: 3 }}>{product.description}</Typography>
+                        <Button variant="contained" sx={{ mr: 1 }}>Add to Cart</Button>
+                        <Button variant="outlined" onClick={() => navigate("/")}>Back</Button>
+                    </Paper>
+            }
         </Container>
     );
 }
