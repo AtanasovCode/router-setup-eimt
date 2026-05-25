dbd6746c1c468cae6df413ffbc55afd411d7e59f
AtanasovCode <a_ljupce@yahoo.com>
2026-05-26 00:00:28 +0200

    convert ProductList to use repository layer

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,39 +1,49 @@
 import { useState, useEffect } from "react";
 import { Container, Grid, Typography } from "@mui/material";
-import instance from "../axios/axios";
+import productsRepository from "../repository/productsRepository";
 import ProductCard from "../components/ProductCard"
 
 
 const ProductList = () => {
 
     const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        instance.get("/products")
+        productsRepository.findAll()
             .then((response) => setProducts(response.data))
             .catch((e) => console.error(e))
+            .finally(() => setLoading(false))
     }, [])
 
     return (
         <Container sx={{ py: 5 }}>
-            <Typography variant="h4" sx={{ mb: 4 }}>
-                Our Products
-            </Typography>
             {
-                products.length > 0 ? (
-                    <Grid container spacing={3}>
-                        {products.map((product) => (
-                            <Grid key={product.id} size={{ xs: 12, sm: 6, md: 3 }}>
-                                <ProductCard product={product} />
-                            </Grid>
-                        ))}
-                    </Grid>
+                loading ? (
+                    <div>Loading...</div>
                 ) : (
                     <div>
-                        No Products Found :(
+                        <Typography variant="h4" sx={{ mb: 4 }}>
+                            Our Products
+                        </Typography>
+                        {
+                            products.length > 0 ? (
+                                <Grid container spacing={3}>
+                                    {products.map((product) => (
+                                        <Grid key={product.id} size={{ xs: 12, sm: 6, md: 3 }}>
+                                            <ProductCard product={product} />
+                                        </Grid>
+                                    ))}
+                                </Grid>
+                            ) : (
+                                <div>
+                                    No Products Found :(
+                                </div>
+                            )
+                        }
                     </div>
                 )
-           }
+            }
         </Container>
     );
 }
